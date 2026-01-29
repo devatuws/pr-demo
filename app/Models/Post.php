8fa092d057dded847181e15012aff06d49d37a56
Nurul Taha <nurultaha.takaful@gmail.com>
2026-01-29 21:02:18 +0800

    Add scopePublished() to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //Add method scopePublished
+    public function scopePublished($query){
+        return $query->where('status', 'published');
+    }
 }
